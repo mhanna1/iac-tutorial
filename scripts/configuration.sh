f3b4dd8a56dda5bf28bde2ff3fe5d7391287447a
mhanna1 <mhanna500@gmail.com>
2018-02-12 11:31:21 -0500

    Added the junk
    Added the junk

---
+++ scripts/configuration.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "  ----- install ruby and bundler -----  "
+apt-get update
+apt-get install -y ruby-full build-essential
+gem install --no-rdoc --no-ri bundler
+
+echo "  ----- install mongodb -----  "
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org
+
+echo "  ----- start mongodb -----  "
+systemctl start mongod
+systemctl enable mongod
+
+echo "  ----- copy unit file for application -----  "
+wget https://gist.github.com/mhanna1/edb663c5d0ca0a2294c174a4ca4bb9b5/raw/fad5bd66141544dc9fb07c13865a9f55e7b03e35/raddit.service
+mv raddit.service /etc/systemd/system/raddit.service
+
